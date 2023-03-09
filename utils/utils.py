f3f4457477ebb1e238cd25f28fb286833f0fc08a
erfanzar <erfanzare82@yahoo.com>
2023-03-09 23:27:54 +0300

    fixing configs

--- utils/utils.py
+++ utils/utils.py
@@ -766,6 +766,35 @@ def get_config_by_name(name: str, vocab_size: int = 5000,
         return LLmPUConfig(
             **L
         )
+    elif name == 'LLmPU-largeX':
+        L = {"d_ff": 2048,
+             "d_kv": 64,
+             "d_model": 768,
+             "decoder_start_token_id": 0,
+             "dropout_rate": 0.1,
+             "eos_token_id": 1,
+             "feed_forward_proj": "gated-gelu",
+             "initializer_factor": 1.0,
+             "is_encoder_decoder": True,
+             "layer_norm_epsilon": 1e-06,
+             "model_type": "t5",
+             "n_positions": 1024,
+             "num_decoder_layers": 14,
+             "num_heads": 12,
+             "num_layers": 14,
+             "output_past": True,
+             "pad_token_id": 0,
+             "relative_attention_max_distance": 128,
+             "relative_attention_num_buckets": 32,
+             "tie_word_embeddings": False,
+             "use_cache": True,
+             "max_length": 768,
+             "mesh": 5,
+             "vocab_size": vocab_size
+             }
+        return LLmPUConfig(
+            **L
+        )
     else:
         raise NameError(
             f"Valid Names for Model are {models_name} | [ERROR : Unknown {name} type]")
