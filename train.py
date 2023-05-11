6e922cb670925e10c69eb1cabc344ec37ffe0b99
erfanzar <erfanzare82@yahoo.com>
2023-05-11 14:10:42 +0330

    Done TODO for `accelerate_train.py`

--- train.py
+++ train.py
@@ -408,7 +408,7 @@ def main(args: Arguments):
         save_steps=args.save_steps,
         save_strategy=args.save_strategy,
         save_total_limit=args.save_total_limit,
-        max_steps=None if args.max_steps == 0 else args.max_steps,
+        max_steps=0 if args.max_steps == 0 else args.max_steps,
         seed=42,
         # fp16=True,
         optim=args.optimizer,
