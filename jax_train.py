ba2eff33da96d70a101eac1b89ba4b904a8cd965
erfanzar <erfanzare82@yahoo.com>
2023-05-23 13:38:06 +0330

    adding utilities for `jax trainer`

--- jax_train.py
+++ jax_train.py
@@ -1,21 +1,62 @@
+import erutils
+import flax.core
 import jax
 from modules import FlaxLGeMForCausalLM, FlaxLGeMConfig
 from transformers import AutoTokenizer
 from datasets import load_dataset
 from dataclasses import dataclass, field
 from typing import Optional
-from transformers import HfArgumentParser, AutoConfig
-from jax import numpy as jnp
+from transformers import HfArgumentParser, AutoConfig, FlaxAutoModelForCausalLM, AutoTokenizer, FlaxPreTrainedModel, \
+    logging
 
-LOCAL_MODELS = [
-    'PaLM',
-    'LGeM'
-]
+from jax import numpy as jnp
+from utils.timer import Timers
+
+logging.set_verbosity_error()
+# jax.config.update()
+jax.default_device = jax.devices('cpu')[0]
+
+
+def prefix_printer(prefix, value):
+    print(f' \033[1;31m{prefix}\033[1;0m : {value}')
+
+
+def get_model_devices(params) -> [str]:
+    devi = {}
+    for pd in jax.tree_util.tree_flatten(flax.core.unfreeze(params))[0]:
+        if isinstance(pd, jnp.DeviceArray):
+            devi[pd.device()] = 'Using'
+    return list(devi.keys())
+
+
+def check_device():
+    print('Device Checking in order ...')
+    dev = {
+        'CPU': jax.devices('cpu'),
+        'GPU': jax.devices('gpu')
+    }
+    none_val_tpu = 'NOT found'
+    try:
+        dev += dict(TPU=jax.devices('tpu'))
+    except RuntimeError:
+
+        dev['TPU'] = none_val_tpu
+    print(f'founded Accelerators on this device are ')
+
+    for k, v in dev.items():
+        prefix_printer(k, v)
+    gpu = jax.devices('gpu')
+    tpu = dev['TPU']
+    prefix_printer('Device Report', f'This machine Contain {len(dev["CPU"])}'
+                                    f' CPUS , {f"have {len(gpu)} GPUS " if len(gpu) != 0 else "have No GPU"} and '
+                                    f'{f"have {len(tpu)} TPU Core" if tpu != none_val_tpu else "have No TPU Core"}')
+    return dev
 
 
 @dataclass
 class TrainingArguments:
     output_dir: str = field(
+        default='OST/JaxModel',
         metadata={"help": "The output directory where the model predictions and checkpoints will be written."},
     )
     overwrite_output_dir: bool = field(
@@ -66,14 +107,6 @@ class ModelArguments:
         metadata={
             "help":
                 "The model checkpoint for weights initialization.Don't set if you want to train a model from scratch."
-
-        },
-    )
-    model_name: Optional[str] = field(
-        default='LGeM',
-        metadata={
-            "help":
-                "Model Name Or Type"
 
         },
     )
@@ -149,27 +182,49 @@ class DataTrainingArguments:
 
 
 def main():
+    check_device()
     arguments = HfArgumentParser((TrainingArguments, ModelArguments, DataTrainingArguments))
     train_args, model_args, data_args = arguments.parse_args_into_dataclasses()
     train_args: TrainingArguments = train_args
     model_args: ModelArguments = model_args
     data_args: DataTrainingArguments = data_args
-    config = AutoConfig.from_pretrained(model_args.config_name) if model_args.config_name is not None else None
 
-    if model_args.model_name in LOCAL_MODELS:
-        if model_args.model_name == 'LGeM':
-            if config is None:
-                assert model_args.model_name
-                model = FlaxLGeMForCausalLM.from_pretrained(
-                    model_args.model_name,
-                    use_auth_token=model_args.use_auth_token,
-                    dtype=jnp.dtype(model_args.dtype),
-                    trust_remote_code=model_args.trust_remote_code
-                )
-            else:
-                model = FlaxLGeMForCausalLM(
-                    config=config
-                )
+    memory_flag = {'tokenizer_name': model_args.tokenizer_name, 'model_name': model_args.model_name_or_path,
+                   'config_from': model_args.config_name, 'dataset_name': data_args.dataset_name,
+                   'dataset_field': data_args.dataset_field, 'block_size|max_sequence_length': data_args.block_size,
+                   'num_train_epochs': train_args.num_train_epochs, 'seed': train_args.seed,
+                   'hub_model_id': train_args.hub_model_id, 'save_steps': train_args.save_steps,
+                   'logging_steps': train_args.logging_steps, 'learning_rate': train_args.learning_rate}
+
+    assert model_args.tokenizer_name, 'tokenizer_name is a required field for trainer ' \
+                                      'please pass a tokenizer path or repo id'
+    prefix_printer('Default Device Jax', jax.default_device)
+    config = AutoConfig.from_pretrained(model_args.config_name,
+                                        trust_remote_code=True,
+
+                                        ) if model_args.config_name is not None else None
+    tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, trust_remote_code=True)
+    assert len(tokenizer.get_vocab()) == config.vocab_size
+
+    if config is None:
+        assert model_args.model_name_or_path
+        model = FlaxAutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            use_auth_token=model_args.use_auth_token,
+            dtype=jnp.dtype(model_args.dtype),
+            trust_remote_code=model_args.trust_remote_code,
+            # revision='main'
+        )
+    else:
+        model: flax.linen.Module | FlaxPreTrainedModel = FlaxAutoModelForCausalLM.from_config(
+            config=config,
+            trust_remote_code=True,
+            # revision='main'
+        )
+
+    _i = jax.tree_util.tree_flatten(flax.core.unfreeze(model.params))[0]
+    prefix_printer('Model Contain ', f'{sum(i.size for i in _i) / 1e6} Million Parameters')
+    prefix_printer('Model Devices Are', f'{get_model_devices(model.params)}')
 
 
 if __name__ == "__main__":
