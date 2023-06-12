c05a574b11278c0b05258be6f56e5bec32aed212
erfanzar <erfanzare82@yahoo.com>
2023-06-12 16:10:46 +0330

    adding `lgem stoper`

--- OST_UI/app.py
+++ OST_UI/app.py
@@ -194,6 +194,8 @@ def chat_bot_run(text: str,
 
     cache_f = cache
     cache_f.append([original_text, ''])
+
+    print(text)
     if model is not None:
         for byte in generate(model, tokenizer, text=text, b_pair=False,
                              generation_config=generation_config, max_stream_tokens=max_new_tokens,
